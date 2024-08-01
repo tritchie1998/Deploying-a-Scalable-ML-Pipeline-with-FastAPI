864f6718bf8f9b999a8a356cf5b344a7badf4ae2
Christopher <tritchietritchie@gmail.com>
2024-08-01 16:27:06 -0600

    updated train model

--- train_model.py
+++ train_model.py
@@ -1,8 +1,7 @@
+import warnings
 import os
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
 from ml.data import process_data
 from ml.model import (
     compute_model_metrics,
@@ -12,15 +11,18 @@ from ml.model import (
     save_model,
     train_model,
 )
+
+warnings.filterwarnings("ignore")
+
 # TODO: load the cencus.csv data
-project_path = "Your path here"
+project_path = ""
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
-data = # your code here
+data = pd.read_csv(data_path)
 
 # TODO: split the provided data to have a train dataset and a test dataset
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = # Your code here
+train, test = train_test_split(data, train_size=0.8, stratify=data["salary"])
 
 # DO NOT MODIFY
 cat_features = [
@@ -36,8 +38,12 @@ cat_features = [
 
 # TODO: use the process_data function provided to process the data.
 X_train, y_train, encoder, lb = process_data(
+    train,
+    categorical_features=cat_features,
+    label="salary",
+    training=True
     # your code here
-    # use the train dataset 
+    # use the train dataset
     # use training=True
     # do not need to pass encoder and lb as input
     )
@@ -52,21 +58,25 @@ X_test, y_test, _, _ = process_data(
 )
 
 # TODO: use the train_model function to train the model on the training dataset
-model = # your code here
+model = train_model(X_train, y_train)  # your code here
 
 # save the model and the encoder
 model_path = os.path.join(project_path, "model", "model.pkl")
 save_model(model, model_path)
+print("Model saved to model/model.pkl")
 encoder_path = os.path.join(project_path, "model", "encoder.pkl")
 save_model(encoder, encoder_path)
+print("Model saved to model/encoder.pkl")
 
 # load the model
 model = load_model(
     model_path
-) 
+)
+
+print("Loading model from model/model.pkl")
 
 # TODO: use the inference function to run the model inferences on the test dataset.
-preds = # your code here
+preds = inference(model, X_test)
 
 # Calculate and print the metrics
 p, r, fb = compute_model_metrics(y_test, preds)
@@ -81,7 +91,15 @@ for col in cat_features:
         p, r, fb = performance_on_categorical_slice(
             # your code here
             # use test, col and slicevalue as part of the input
+            data=test,
+            column_name=col,
+            slice_value=slicevalue,
+            categorical_features=cat_features,
+            label="salary",
+            encoder=encoder,
+            lb=lb,
+            model=model
         )
         with open("slice_output.txt", "a") as f:
-        print(f"{col}: {slicevalue}, Count: {count:,}", file=f)
-        print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}", file=f)
+            print(f"{col}: {slicevalue}, Count: {count:,}", file=f)
+            print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}", file=f)
